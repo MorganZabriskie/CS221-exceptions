9228a7ca78bd6d737c803febd18d02b7c7a7c7e4
Morgan Zabriskie <morganzabriskie@u.boisestate.edu>
2024-09-12 17:01:37 -0600

    Working on file check method

--- FormatChecker.java
+++ FormatChecker.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FormatChecker {
     //instance variables
@@ -20,9 +21,40 @@ public class FormatChecker {
         //check format method
     private boolean validCheck(File filename) throws FileNotFoundException {
         boolean valid = false;
-
+        int rows = 0;
+        int cols = 0;
         
+        if(filename.exists() && filename.isFile()) {
+            Scanner scan = new Scanner(filename);
+            String firstRow = scan.nextLine();
+            Scanner paramScan = new Scanner(firstRow);
+            paramScan.useDelimiter("\s");
 
+            //scan for first row (two numbers) and find # of rows and columns
+            rows = Integer.parseInt(paramScan.next());
+            if (rows < 0) {
+                System.out.println("Please enter a positive value for number of rows.");
+                System.exit(1);
+            }
+            cols = Integer.parseInt(paramScan.next());
+            if (cols < 0) {
+                System.out.println("Please enter a positive value for number of columns.");
+            }
+
+            paramScan.close();
+
+            //scan rest of file to check correct amount of rows/cols and all doubles
+            Double[][] fileCheck = new Double[rows][cols];
+            for(int i = 0; i < fileCheck.length; i++) {
+                String rowScan = scan.nextLine();
+                Scanner lineScan = new Scanner(rowScan);
+                lineScan.useDelimiter("\s");
+                for (int j = 0; j < fileCheck[0].length; j++) {
+                    fileCheck[i][j] = Double.parseDouble(lineScan.next());
+                }
+                lineScan.close();
+            }
+        }
         return valid;
     }
 
